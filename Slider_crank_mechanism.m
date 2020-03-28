3ba8160dd1f4b2155a0d24a84f4ff4df85ca5914
PetLak <petr.p.lakomy@student.lab.fi>
2020-03-28 16:47:46 +0200

    While loop was added.
    Position and velocity is computed and plotter for each time in array.

--- Slider_crank_mechanism.m
+++ Slider_crank_mechanism.m
@@ -1,19 +1,49 @@
-a = 0.1; b = 0.2; fi_d = 30; omega =  -1;
-fi_r = pi * fi_d/180; fi_r_diff = omega;
+a = 0.1; b = 0.2; omega =  -1;
+fi_r_diff = omega; fi_r = @(t) pi/6+omega*t;
 
-F = @(x) [a*cos(fi_r)+b*cos(x(1))-x(2);...
-          a*sin(fi_r)-b*sin(x(1))];
+t_init = 0; % Initial time position.
+t_end = 10; % Time at the end.
+step = 0.02; % Step time for computing and plotting.
+tol = 1e-8; % Tolerance for computing.
 
-J = @(x) [-b*sin(x(1)), -1;...
-          -b*cos(x(1)),  0];
+init_est = [0.5; 1]; % Initial estimate.
 
-init_est = [0.5; 0.3];
-tol = 1e-12;
+t_plot = t_init:step:t_end; % time array.
+n_s = length(t_plot); % Number of solution.
 
-[x, n] = NR_method(F, J, init_est, tol);
 
-theta_diff = (a*cos(fi_r)*fi_r_diff)/(0.2*cos(x(1)));
-d_diff = -a*sin(fi_r)*fi_r_diff-b*sin(x(1))*theta_diff;
+x = zeros(2,n_s); % Matrix for storing solution.
 
-fprintf('theta = %.5g° (%.5g rad), d = %.5g m\n', 180*x(1)/pi, x(1), x(2))
-fprintf('theta_velocity = %.5g rad/s, d_velocity = %.5g m/s\n', theta_diff, d_diff)
+ii = 1;
+
+while t_init <= t_end
+    F = @(x) [a*cos(fi_r(t_init))+b*cos(x(1))-x(2);...
+              a*sin(fi_r(t_init))-b*sin(x(1))];
+
+    J = @(x) [-b*sin(x(1)), -1;...
+              -b*cos(x(1)),  0];
+
+          x_ss = NR_method(F, J, init_est, tol);
+          init_est = x_ss;
+          x(:,ii)=x_ss;
+
+          t_init = t_init + step;
+          ii = ii + 1; 
+end
+
+hold on
+plot(t_plot,x(1,:),'r-', 'LineWidth', 1.4)
+plot(t_plot,x(2,:),'b-', 'LineWidth', 1.4)
+
+fi_r_vec = pi/6+omega*t_plot;
+
+theta_diff = (a*cos(fi_r_vec)*fi_r_diff)./(0.2*cos(x(1,:)));
+d_diff = -a*sin(fi_r_vec)*fi_r_diff-b*sin(x(1,:)).*theta_diff;
+
+plot(t_plot,theta_diff,'c-', 'LineWidth', 1.4)
+plot(t_plot,d_diff,'g-', 'LineWidth', 1.4)
+title('Position and Velocity')
+xlabel('t')
+ylabel('\theta [rad], d [m],\theta velocity [rad/s], d velocity [m/s]')
+legend('\theta position','d position','\theta velocity', 'd velocity')
+hold off
