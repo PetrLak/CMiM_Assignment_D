f99332115b3d37ef3bd8e4a08e267c71e8632498
PetLak <petr.p.lakomy@student.lab.fi>
2020-03-28 15:20:02 +0200

    File Slider_crank_mechanism.m was added.
    The mechanism is solved for one specific position with the same parameters as were provided in CMiM 11. The results are the same - at this stage the script should be alright.

---
+++ Slider_crank_mechanism.m
@@ -0,0 +1,16 @@
+a = 0.1; b = 0.2; fi_d = 30; omega =  -1;
+fi_r = pi * fi_d/180; fi_r_diff = omega;
+
+F = @(x) [a*cos(fi_r)+b*cos(x(1))-x(2);...
+          a*sin(fi_r)-b*sin(x(1))];
+
+J = @(x) [-b*sin(x(1)), -1;...
+          -b*cos(x(1)),  0];
+
+init_est = [0.5; 0.3];
+tol = 1e-12;
+
+[x, n] = NR_method(F, J, init_est, tol)
+
+theta_diff = (a*cos(fi_r)*fi_r_diff)/(0.2*cos(x(1)))
+d_diff = -a*sin(fi_r)*fi_r_diff-b*sin(x(1))*theta_diff
